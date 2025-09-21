aec607a4565ff81924ae3a7e0e2295b0443d8224
Lucasion <lucasion@hotmail.com>
2025-09-21 23:39:23 +0000

    feat: Remove unnecessary JVM options for NeoForge server setup

--- server/services/ServerSetupService.ts
+++ server/services/ServerSetupService.ts
@@ -360,16 +360,6 @@ export default class ServerSetupService {
       `-Xmx${memory}M`,
       '',
       '# Additional JVM optimizations for modded servers',
-      '-XX:+UseG1GC',
-      '-XX:+UnlockExperimentalVMOptions',
-      '-XX:MaxGCPauseMillis=100',
-      '-XX:+DisableExplicitGC',
-      '-XX:TargetSurvivorRatio=90',
-      '-XX:G1NewSizePercent=50',
-      '-XX:G1MaxNewSizePercent=80',
-      '-XX:G1MixedGCLiveThresholdPercent=35',
-      '-XX:+AlwaysPreTouch',
-      '-XX:+UseLargePagesInMetaspace',
       ''
     ].join('\n');
     
